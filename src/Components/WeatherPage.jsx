df636a9e1b6ad347f151fd1636309345d4425781
deeparka <arkadeep180@gmail.com>
2022-06-06 14:11:30 +0530

    completed basic of weather page and ongoing css

--- src/Components/WeatherPage.jsx
+++ src/Components/WeatherPage.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { BsSun } from "react-icons/bs";
-import { useLocation } from "react-router-dom";
+import { GoSearch } from "react-icons/go";
+import {
+    BsSun,
+    BsFillArrowUpCircleFill,
+    BsFillArrowDownCircleFill,
+} from "react-icons/bs";
+import { BiTargetLock } from "react-icons/bi";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function WeatherPage() {
     const location = useLocation();
-    const { city } = location.state;
     // console.log(location);
-    // const [cityLocation, setCityLocation] = useState(``);
+    const { city } = location.state;
+
+    const navigate = useNavigate();
+
+    const [cityName, setCityName] = useState(``);
+
     const [country, setCountry] = useState(``);
     const [time, setTime] = useState(``);
     const [temperatureCelsius, setTemperatureCelsius] = useState(0);
     const [temperatureFarenheit, setTemperatureFarenheit] = useState(0);
     const [weatherCondition, setWeatherCondition] = useState(``);
+    const [uvIndex, setUVIndex] = useState(0);
+    const [sunRise, setSunRise] = useState(``);
+    const [sunSet, setSunSet] = useState(``);
     const [humidity, setHumidity] = useState(0);
     const [windSpeed, setWindSpeed] = useState(0);
+    const [windDirection, setWindDirection] = useState(``);
+    const [visibility, setVisibility] = useState(0);
+    const [airQuality, setAirQuality] = useState(0);
 
     useEffect(() => {
         const weather = async () =>
@@ -28,11 +44,26 @@ function WeatherPage() {
                     setTemperatureCelsius(data.current.temp_c);
                     setTemperatureFarenheit(data.current.temp_f);
                     setWeatherCondition(data.current.condition.text);
+                    setUVIndex(data.current.uv);
                     setHumidity(data.current.humidity);
+                    setSunRise(data.forecast.forecastday[0].astro.sunrise);
+                    setSunSet(data.forecast.forecastday[0].astro.sunset);
                     setWindSpeed(data.current.wind_kph);
+                    setWindDirection(data.current.wind_dir);
+                    setVisibility(data.current.vis_km);
+                    setAirQuality(data.current.air_quality.co);
                 });
         weather();
     }, [city]);
+
+    const showWeather = (e) => {
+        e.preventDefault();
+        navigate("/weather", {
+            state: {
+                city: cityName,
+            },
+        });
+    };
 
     const getStandardTimes = (time) => {
         const utcSeconds = time;
@@ -47,7 +78,10 @@ function WeatherPage() {
         let hours = dt.getHours(); // gives the value in 24 hours format
         const AmOrPm = hours >= 12 ? "PM" : "AM";
         hours = hours % 12 || 12;
-        const minutes = dt.getMinutes();
+        let minutes = dt.getMinutes();
+        if (minutes.count === 1) {
+            minutes = "0" + minutes;
+        }
         const finalTime = hours + ":" + minutes + " " + AmOrPm;
         // console.log(finalTime);
         return finalTime;
@@ -95,22 +129,28 @@ function WeatherPage() {
     return (
         <div className="weather-container">
             <div className="left">
-                <div className="city">
-                    <div className="your-city">Your City</div>
-                    <div className="city-name">{city}</div>
-                </div>
-                <div className="country">
-                    <div className="your-country">Country</div>
-                    <div className="country-name">{country}</div>
-                </div>
-                <div className="date-time">
-                    <div className="time">{twelveHourTime(time)}</div>
-                    <div className="day-of-week">{getDayOfWeek(time)}</div>
-                    <div className="date">{getDate(time)}</div>
+                <div className="input-city-name-btn">
+                    <div className="input-city-name">
+                        <GoSearch className="search-icon" />
+                        <input
+                            type="text"
+                            placeholder="Enter City Name"
+                            className="city"
+                            value={cityName}
+                            onChange={(e) => setCityName(e.target.value)}
+                        />
+                    </div>
+                    <div className="btn-class">
+                        <form action="" onSubmit={showWeather}>
+                            <button type="submit" className="submit-btn-class">
+                                <BiTargetLock className="target" />
+                            </button>
+                        </form>
+                    </div>
                 </div>
                 <div className="weather-temp">
                     <div className="weather-icon">
-                        <BsSun />
+                        <BsSun className="icon" />
                     </div>
                     <div className="temperature">
                         <div className="temperature-celsius">
@@ -122,17 +162,65 @@ function WeatherPage() {
                         </div>
                     </div>
                 </div>
+                <div className="week-time">
+                    <div className="day-of-week">{getDayOfWeek(time)},</div>
+                    <div className="time">{twelveHourTime(time)}</div>
+                </div>
+                <div className="date">{getDate(time)}</div>
+                <div className="city-text-name">
+                    <div className="city-text">City</div>
+                    <div>:</div>
+                    <div className="city-name">{city}</div>
+                </div>
+                <div className="country">
+                    <div className="your-country">Country:</div>
+                    <div className="country-name">{country}</div>
+                </div>
+
                 <div className="condition">{weatherCondition}</div>
-                <div className="humidity">
-                    <div className="humidity-text">Humidity</div>
-                    <div className="humidity-percentage">{humidity}%</div>
+            </div>
+            <div className="right">
+                <div className="today-status">Today's Status</div>
+                <div className="uv">
+                    <div className="uv-index">UV Index</div>
+                    <div className="uv-value">{uvIndex}</div>
                 </div>
                 <div className="wind">
                     <div className="wind-text">Wind Speed</div>
                     <div className="wind-speed">{windSpeed}Km/h</div>
+                    <div className="wind-direction">{windDirection}</div>
+                </div>
+                <div className="sunrise-sunset">
+                    <div className="sunrise-sunset-text">Sunrise & Sunset</div>
+                    <div className="sunrise-up">
+                        <div className="up">
+                            <BsFillArrowUpCircleFill />
+                        </div>
+                        <div className="sunrise">{sunRise}</div>
+                    </div>
+                    <div className="sunset-down">
+                        <div className="down">
+                            <BsFillArrowDownCircleFill />
+                            <div className="sunset">{sunSet}</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="humidity">
+                    <div className="humidity-text">Humidity</div>
+                    <div className="humidity-percentage">{humidity}%</div>
+                    <div className="humdity-description">Bad</div>
+                </div>
+                <div className="visibility">
+                    <div className="visibility-text">Visibility</div>
+                    <div className="visible-km">{visibility}Km</div>
+                    <div className="visibility-description">Average</div>
+                </div>
+                <div className="air-quality">
+                    <div className="air-quality-text">Air Quality</div>
+                    <div className="air">{Math.round(airQuality)}</div>
+                    <div className="air-condition">Bad</div>
                 </div>
             </div>
-            <div className="right"></div>
         </div>
     );
 }
